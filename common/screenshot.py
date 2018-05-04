53269321500d9ab42545190146da6a5937e3ccf1
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-05-04 22:47:54 +0800

    yapf

--- common/screenshot.py
+++ common/screenshot.py
@@ -5,8 +5,7 @@
 import subprocess
 import os
 import sys
-from PIL import Image,ImageFile
-
+from PIL import Image, ImageFile
 
 # SCREENSHOT_WAY 是截图方法，经过 check_screenshot 后，会自动递减，不需手动修改
 SCREENSHOT_WAY = 3
@@ -20,8 +19,7 @@ def pull_screenshot():
     global SCREENSHOT_WAY
     if 1 <= SCREENSHOT_WAY <= 3:
         process = subprocess.Popen(
-            'adb shell screencap -p',
-            shell=True, stdout=subprocess.PIPE)
+            'adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
         binary_screenshot = process.stdout.read()
         if SCREENSHOT_WAY == 2:
             binary_screenshot = binary_screenshot.replace(b'\r\n', b'\n')
@@ -32,7 +30,7 @@ def pull_screenshot():
         f.write(binary_screenshot)
         f.close()
         '''
-        f=ImageFile.Parser()
+        f = ImageFile.Parser()
         f.feed(binary_screenshot)
         return f.close()
     elif SCREENSHOT_WAY == 0:
@@ -55,7 +53,7 @@ def check_screenshot():
         print('暂不支持当前设备,请检查手机adb是否已经打开并授权')
         sys.exit()
     try:
-        im=pull_screenshot()
+        im = pull_screenshot()
         print('采用方式 {} 获取截图'.format(SCREENSHOT_WAY))
     except:
         SCREENSHOT_WAY -= 1
--- evol_auto.py
+++ evol_auto.py
@@ -16,15 +16,17 @@ import subprocess
 from PIL import Image
 #from six.moves import input
 from common import screenshot
-global talkcount,markx,marky,lastspec
-talkcount=0
+global talkcount, markx, marky, lastspec
+talkcount = 0
+
+
 def yes_or_no(prompt, true_value='y', false_value='n', default=True):
     """
     prompt connect adb
     """
     default_value = true_value if default else false_value
-    prompt = '{} {}/{} [{}]: '.format(prompt, true_value,
-        false_value, default_value)
+    prompt = '{} {}/{} [{}]: '.format(prompt, true_value, false_value,
+                                      default_value)
     i = input(prompt)
     if not i:
         return default
@@ -35,7 +37,16 @@ def yes_or_no(prompt, true_value='y', false_value='n', default=True):
             return False
         prompt = 'Please input {} or {}: '.format(true_value, false_value)
         i = input(prompt)
-def pixel_match(im,target_x,target_y,target_r,target_g,target_b,diff,debug=False):
+
+
+def pixel_match(im,
+                target_x,
+                target_y,
+                target_r,
+                target_g,
+                target_b,
+                diff,
+                debug=False):
     """
     try:
         im_pixel=im.load()
@@ -44,98 +55,125 @@ def pixel_match(im,target_x,target_y,target_r,target_g,target_b,diff,debug=False
     pixel=im_pixel[target_x,target_y]
         old method
     """
-    pixel=im.getpixel((target_x,target_y))
+    pixel = im.getpixel((target_x, target_y))
     if debug:
-        print([target_x,target_y],pixel,[target_r,target_g,target_b])
-    if abs(pixel[0]-target_r)+abs(pixel[1]-target_g)+abs(pixel[2]-target_b)<=diff:
+        print([target_x, target_y], pixel, [target_r, target_g, target_b])
+    if abs(pixel[0] - target_r) + abs(pixel[1] - target_g) + abs(
+            pixel[2] - target_b) <= diff:
         return True
     else:
         return False
+
+
 def detect_selection(im):
     if detect_main_menu(im):
         return False
-    for cy in range(192,1000):
-        if pixel_match(im,220,cy,250,125,147,30) and pixel_match(im,680,cy,250,125,147,30) and pixel_match(im,890,cy,250,125,147,30):
+    for cy in range(192, 1000):
+        if pixel_match(im, 220, cy, 250, 125, 147, 30) and pixel_match(
+                im, 680, cy, 250, 125, 147, 30) and pixel_match(
+                    im, 890, cy, 250, 125, 147, 30):
             return True
     return False
+
+
 def detect_loc(im):
-    global markx,marky
+    global markx, marky
     if not detect_main_menu(im):
         return False
-    for x in range(400,1000):
-        for y in range(480,1000):
-            if pixel_match(im,x,y,220,115,140,30) and pixel_match(im,x,y+50,220,115,140,30) and (not pixel_match(im,x,y+200,220,115,140,30)):
-                    markx=x
-                    marky=y
-                    return True
+    for x in range(400, 1000):
+        for y in range(480, 1000):
+            if pixel_match(im, x, y, 220, 115, 140, 30) and pixel_match(
+                    im, x, y + 50, 220, 115, 140, 30) and (not pixel_match(
+                        im, x, y + 200, 220, 115, 140, 30)):
+                markx = x
+                marky = y
+                return True
     return False
+
+
 def detect_comp(im):
     global routcount
-    return ((routcount>20) and detect_main_menu(im))
+    return ((routcount > 20) and detect_main_menu(im))
+
+
 def do_talk():
-    for i in range(0,10):
-        tap(321+drift(),1443+drift())
+    for i in range(0, 10):
+        tap(321 + drift(), 1443 + drift())
+
+
 def in_talk(im):
     global talkcount
-    talkcount+=1
-    return (not detect_main_menu(im)) and (pixel_match(im,912,1819,247,128,151,80))
+    talkcount += 1
+    return (not detect_main_menu(im)) and (pixel_match(im, 912, 1819, 247, 128,
+                                                       151, 80))
+
+
 def detect_main_menu(im):
-    matchdiamond=False
-    matchplus=False
-    for y in range(0,100):
-        for x in range(760,950):
-            if pixel_match(im,x,y,187,30,72,3):
-                matchdiamond=True
-        for x in range(970,1080):
-            if pixel_match(im,x,y,189,156,130,3):
-                matchplus=True
+    matchdiamond = False
+    matchplus = False
+    for y in range(0, 100):
+        for x in range(760, 950):
+            if pixel_match(im, x, y, 187, 30, 72, 3):
+                matchdiamond = True
+        for x in range(970, 1080):
+            if pixel_match(im, x, y, 189, 156, 130, 3):
+                matchplus = True
     return (matchdiamond and matchplus)
+
+
 def drift():
-    return random.uniform(10,30)
-def tap(px,py):
-    global lastspec,need_resize,height,width
-    if not (px==956):
-        lastspec=True
+    return random.uniform(10, 30)
+
+
+def tap(px, py):
+    global lastspec, need_resize, height, width
+    if not (px == 956):
+        lastspec = True
     else:
-        lastspec=False
+        lastspec = False
     if need_resize:
-        px*=width
-        py*=height
+        px *= width
+        py *= height
     si = subprocess.STARTUPINFO()
     si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
     #si.wShowWindow = subprocess.SW_HIDE # default
-    cmd = 'adb shell input tap {x} {y}'.format(
-        x=px,
-        y=py
-    )
+    cmd = 'adb shell input tap {x} {y}'.format(x=px, y=py)
     #print(cmd)
     #print(talkcount)
     subprocess.Popen(cmd, startupinfo=si)
     #os.system(cmd)
     time.sleep(0.1)
+
+
 def do_sel():
     print("selecting...")
-    tap(781,690-drift())
+    tap(781, 690 - drift())
     time.sleep(2)
+
+
 def sel_loc():
     print("arrived...")
-    tap(markx+10+drift(),marky+drift())
+    tap(markx + 10 + drift(), marky + drift())
     time.sleep(2)
+
+
 def do_3sel(im):
     global talkcount
     print("3sel!!!")
     while not detect_main_menu(im):
         im.close()
-        im=do_screenshot()
+        im = do_screenshot()
         for i in range(10):
-            tap(956,1870)
+            tap(956, 1870)
             time.sleep(0.1)
         time.sleep(1.5)
-        tap(random.choice((200,540,880)),750+drift())
+        tap(random.choice((200, 540, 880)), 750 + drift())
         time.sleep(3)
-    talkcount=0
+    talkcount = 0
+
+
 def Determine_status(im):
-    global talkcount,routcount
+    global talkcount, routcount
     if detect_selection(im) and (not lastspec):
         do_sel()
     elif detect_loc(im) and (not lastspec):
@@ -145,78 +183,82 @@ def Determine_status(im):
         print("Current process is complete!")
         sys.exit()
     elif in_talk(im):
-        routcount=0
+        routcount = 0
         print("Talk...")
         do_talk()
     else:
-        talkcount=0
-        routcount+=1
+        talkcount = 0
+        routcount += 1
         print("routine...")
         for i in range(10):
-            tap(956,1870)
+            tap(956, 1870)
             time.sleep(0.1)
+
+
 def do_screenshot():
-    global need_resize,need_rotate,height,width
-    print("Pull_screenshot...",end=" ")
+    global need_resize, need_rotate, height, width
+    print("Pull_screenshot...", end=" ")
     try:
         im = screenshot.pull_screenshot()
-        success=True
+        success = True
     except:
-        success=False
+        success = False
         try:
             im = screenshot.pull_screenshot()
-            success=True
+            success = True
         except:
             print("screenshot_error")
     if success:
         if need_rotate:
-            print("Rotating...",end=" ")
-            im=im.transpose(Image.ROTATE_90)
-            width=width+height
-            height=width-height
-            width=width-height
+            print("Rotating...", end=" ")
+            im = im.transpose(Image.ROTATE_90)
+            width = width + height
+            height = width - height
+            width = width - height
         if need_resize:
-            print("Resizing...",end=" ")
-            im=im.resize((1080,1920))
+            print("Resizing...", end=" ")
+            im = im.resize((1080, 1920))
     return im
+
+
 def main():
-    global talkcount,routcount,need_resize,need_rotate,height,width
-    op = yes_or_no('remote adb?',default=False)
+    global talkcount, routcount, need_resize, need_rotate, height, width
+    op = yes_or_no('remote adb?', default=False)
     if op:
-        netpos=input()
-        cmd = 'adb connect {x}'.format(
-            x=netpos,
-        )
+        netpos = input()
+        cmd = 'adb connect {x}'.format(x=netpos, )
         os.system(cmd)
-    sel=0
+    sel = 0
     #debug.dump_device_info()
     screenshot.check_screenshot()
-    im=screenshot.pull_screenshot()
-    width,height=im.size
-    need_rotate=False
-    need_resize=False
-    if width>height:
-            print("Need rotate!")
-            need_rotate=True
-            im=im.transpose(Image.ROTATE_90)
-            width=width+height
-            height=width-height
-            width=width-height
-    if (width!=1080) or (height!=1920):
-            print("Need resize!")
-            need_resize=True
+    im = screenshot.pull_screenshot()
+    width, height = im.size
+    need_rotate = False
+    need_resize = False
+    if width > height:
+        print("Need rotate!")
+        need_rotate = True
+        im = im.transpose(Image.ROTATE_90)
+        width = width + height
+        height = width - height
+        width = width - height
+    if (width != 1080) or (height != 1920):
+        print("Need resize!")
+        need_resize = True
     im.close()
-    width=width/1080
-    height=height/1920
-    tap(956,1870)
-    talkcount=0
-    routcount=0
+    width = width / 1080
+    height = height / 1920
+    tap(956, 1870)
+    talkcount = 0
+    routcount = 0
     while True:
-        im=do_screenshot()
-        print("Parsing...",end=" ")
+        im = do_screenshot()
+        print("Parsing...", end=" ")
         Determine_status(im)
-        if (not detect_main_menu(im)) and ((talkcount>=15)):  
+        if (not detect_main_menu(im)) and ((talkcount >= 15)):
             do_3sel(im)
         #time.sleep(random.uniform(0.1, 0.5))
         im.close()
+
+
 main()
